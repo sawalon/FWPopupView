4e74cc09b77c8f55d02830a8eb6a1407f07789be
sawalon.chen <sawalon.chen@ui.com>
2022-02-25 15:45:27 +0800

    [feat] support spm

--- FWPopupViewOCDemo/Pods/Target Support Files/FWPopupViewOC/FWPopupViewOC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FWPopupViewOC : NSObject
-@end
-@implementation PodsDummy_FWPopupViewOC
-@end
--- FWPopupViewSwiftDemo/Pods/Target Support Files/Pods-FWPopupView/Pods-FWPopupView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FWPopupView : NSObject
-@end
-@implementation PodsDummy_Pods_FWPopupView
-@end
